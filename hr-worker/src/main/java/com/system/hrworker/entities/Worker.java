f5a22724c7149b4050fde4fcc594d98805e878d5
Victor Tavares <victor.as156@gmail.com>
2021-02-03 00:48:16 -0300

    Project hr-payroll created

--- hr-worker/src/main/java/com/system/hrworker/entities/Worker.java
+++ hr-worker/src/main/java/com/system/hrworker/entities/Worker.java
@@ -11,11 +11,9 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "tb_worker")
 public class Worker implements Serializable{
-	
-	
 	private static final long serialVersionUID = 1L;
-	//ATTRIBUTES
 	
+	//ATTRIBUTES
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
