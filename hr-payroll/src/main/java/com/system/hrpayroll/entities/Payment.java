c95287a455353e1258814cb9c088b62e4e4ab6b2
Victor Tavares <victor.as156@gmail.com>
2021-02-03 01:06:25 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/system/hrpayroll/entities/Payment.java
@@ -0,0 +1,57 @@
+package com.system.hrpayroll.entities;
+
+import java.io.Serializable;
+
+public class Payment implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	//ATTRIBUTES
+	private String name;
+	private Double dailyIncome;
+	private Integer days;
+	
+	
+	//CONSTRUCTORS
+	public Payment() {
+	}
+
+	public Payment(String name, Double dailyIncome, Integer days) {
+		super();
+		this.name = name;
+		this.dailyIncome = dailyIncome;
+		this.days = days;
+	}
+
+	
+	//GETTERS AND SETTERS
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getDailyIncome() {
+		return dailyIncome;
+	}
+
+	public void setDailyIncome(Double dailyIncome) {
+		this.dailyIncome = dailyIncome;
+	}
+
+	public Integer getDays() {
+		return days;
+	}
+
+	public void setDays(Integer days) {
+		this.days = days;
+	}
+	
+	
+	//METHODS
+	public double getTotal() {
+		return days * dailyIncome;
+	}
+
+}
---
+++ hr-payroll/src/main/java/com/system/hrpayroll/services/PaymentService.java
@@ -0,0 +1,13 @@
+package com.system.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.system.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+
+	public Payment getPayment(long workerId, int days) {
+		return new Payment("Bob", 200.0, days);
+	}
+}
